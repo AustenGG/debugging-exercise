aaefc14d75831852d82181dfa7649b6c6b829553
EC2 Default User <ec2-user@ip-172-31-6-62.us-east-2.compute.internal>
2018-10-03 10:40:44 +0000

    Exercise 2

---
+++ debugging.rb
@@ -0,0 +1,33 @@
+
+# ORIIGNAL EXERCISE 2
+# def encode(plaintext, key)
+#  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+#  ciphertext_chars = plaintext.chars.map do |char|
+#    (65 + cipher.find_index(char)).chr
+#  end
+#  ciphertext_chars.join
+# end
+
+# def decode(ciphertext, key)
+#  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+#  plaintext_chars = ciphertext.chars.map do |char|
+#    cipher[65 - char.ord]
+#  end
+#  plaintext_chars.join
+# end
+
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+   (65 + cipher.find_index(char).to_i).chr
+  end
+  ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+  cipher[char.ord-65]
+  end
+  plaintext_chars.join
+end
